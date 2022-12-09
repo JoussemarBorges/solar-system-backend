493f5e7d1ca9cc1ad5127b88e38e94407340e9dd
Joussemar Borges <joussemar.borges@gmail.com>
2022-12-09 17:03:27 -0300

    feat: Criação do index e da função de leitura do arquivo missions

---
+++ src/utils/fsUtils.js
@@ -0,0 +1,18 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readMissionsData() {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, '../../data/missions.json'))
+    const missions = JSON.parse(data);
+    return missions;
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`)
+  }
+}
+
+readMissionsData();
+
+module.exports = {
+  readMissionsData
+};
