5c2f50063caa8787606414fa8f8670c620069f2b
Joussemar Borges <joussemar.borges@gmail.com>
2022-12-09 18:14:56 -0300

    feat: adiciona uma nova missão através do readline

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
+const readline = require('readline-sync')
 const { readMissionsData, writeNewMissionData } = require("./utils/fsUtils");
 
 async function main() {
- writeNewMissionData({
-    "id": 100,
-    "name": "Tryber 1",
-    "year": "2004",
-    "country": "USA",
-    "destination": "Mercúrio"
- })
+  const name = readline.question('Qual é o nome da missão? ');
+  const year = readline.questionInt('Qual é o ano da missão? ');
+  const  country = readline.question('Qual é o país da missão? ');
+  const destination = readline.question('Qual é o destino da missão? ');
+
+
+  const newMission = { name, year, country, destination }
+  writeNewMissionData(newMission);
+
+  console.log('Nova missão registrada com sucesso');
+//  writeNewMissionData({
+//     "id": 100,
+//     "name": "Tryber 1",
+//     "year": "2004",
+//     "country": "USA",
+//     "destination": "Mercúrio"
+//  })
 }
 
 main();
