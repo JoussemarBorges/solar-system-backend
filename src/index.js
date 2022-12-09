38779c920c10b0d2a74313fc056b0a12baa20664
Joussemar Borges <joussemar.borges@gmail.com>
2022-12-09 18:17:50 -0300

    feat: configurando o lintc

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-const readline = require('readline-sync')
-const { readMissionsData, writeNewMissionData } = require("./utils/fsUtils");
+const readline = require('readline-sync');
+const { writeNewMissionData } = require('./utils/fsUtils');
 
 async function main() {
   const name = readline.question('Qual é o nome da missão? ');
   const year = readline.questionInt('Qual é o ano da missão? ');
-  const  country = readline.question('Qual é o país da missão? ');
+  const country = readline.question('Qual é o país da missão? ');
   const destination = readline.question('Qual é o destino da missão? ');
 
-
-  const newMission = { name, year, country, destination }
+  const newMission = { name, year, country, destination };
   writeNewMissionData(newMission);
 
   console.log('Nova missão registrada com sucesso');
