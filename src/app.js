62268523cad193063189b4df6f67241eca6b4495
Joussemar Borges <joussemar.borges@gmail.com>
2022-12-09 21:17:06 -0300

    feat: Finalizando a primeira API REST com CRUD completo \o/

--- src/app.js
+++ src/app.js
@@ -1,5 +1,54 @@
 const express = require('express');
+const { readMissionsData,
+        writeNewMissionData,
+        updateMissionData,
+        deleteMissionData,
+      } = require('./utils/fsUtils');
 
 const app = express();
+
+app.use(express.json());
+
+const OK = 200;
+const CREATED = 201;
+const DELETED = 204;
+
+app.get('/missions/', async (req, res) => {
+  try {
+    const missions = await readMissionsData();
+
+    return res.status(OK).json({ missions });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post('/missions', async (req, res) => {
+  try {
+    const newMission = req.body;
+    const newMissionWhitId = await writeNewMissionData(newMission);
+    
+    return res.status(CREATED).json({ mission: newMissionWhitId });
+  } catch (error) {
+    console.log({ message: 'deu ruim' });
+  }
+});
+
+app.put('/missions/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedMissionData = req.body;
+
+  const updatedMission = await updateMissionData(Number(id), updatedMissionData);
+
+  return res.status(CREATED).json({ mission: updatedMission });
+});
+
+app.delete('/missions/:id', async (req, res) => {
+  const { id } = req.params;
+
+  await deleteMissionData(id);
+
+  return res.status(DELETED).end();
+});
 
 module.exports = app;
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -16,17 +16,51 @@ async function readMissionsData() {
 async function writeNewMissionData(newMission) {
   try {
     const oldMissions = await readMissionsData();
-    const allMissions = JSON.stringify([...oldMissions,
-       { id: Date.now(), ...newMission },
-    ]);
+    const newMissionWhitId = { id: Date.now(), ...newMission };
+    const allMissions = JSON.stringify([...oldMissions, newMissionWhitId]);
 
     await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions);
+
+    return newMissionWhitId;
   } catch (error) {
     console.error(`Erro na leitura do arquivo: ${error}`);
   }
 }
 
+async function updateMissionData(id, updatedMissionData) {
+  const oldMissions = await readMissionsData();
+  const updatedMission = { id, ...updatedMissionData };
+
+  const updatedMissions = oldMissions.reduce((missionsList, currentMission) => {
+    if (currentMission.id === updatedMission.id) return [...missionsList, updatedMission];
+    return [...missionsList, currentMission];
+  }, []);
+
+  const updatedData = JSON.stringify(updatedMissions);
+
+  try {
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), updatedData);
+    console.log(`Atualizou a missão com o id ${id}`);
+    return updatedMission;
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`);
+  }
+}
+
+async function deleteMissionData(id) {
+  const oldMissions = await readMissionsData();
+  const updatedMissions = oldMissions.filter((mission) => mission.id !== Number(id));
+
+  const updateData = JSON.stringify(updatedMissions);
+  try {
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), updateData);
+  } catch (error) {
+    console.error(`Erro na leitura do arquivo: ${error}`);
+  }
+}
 module.exports = {
   readMissionsData,
   writeNewMissionData,
+  updateMissionData,
+  deleteMissionData,
 };
