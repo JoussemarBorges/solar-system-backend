78247e6dc40687b3be0c9027b27ce344ce795e65
Joussemar Borges <joussemar.borges@gmail.com>
2022-12-12 12:37:52 -0300

    feat: utilizando o sinon e stubs com before e after each para simular chamadada de funções

--- tests/integration/mission.test.js
+++ tests/integration/mission.test.js
@@ -1,5 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const fs = require('fs');
 
 chai.use(chaiHttp);
 
@@ -7,26 +10,42 @@ const { expect } = chai;
 
 const app = require('../../src/app');
 
+const mockMissions = JSON.stringify([
+  { id: 1, name: 'Mariner 2', year: '1962', country: 'USA', destination: 'Vênus' },
+  { id: 2, name: 'Venera 4', year: '1967', country: 'URSS', destination: 'Vênus' },
+  { id: 3, name: 'Mariner 5', year: '1967', country: 'USA', destination: 'Vênus' },
+]);
+
 describe('Rota de Missões', function () {
   describe('GET /missions', function () {
     it('Retorna uma lista de missões', async function () {
+      sinon.stub(fs.promises, 'readFile')
+      .resolves(mockMissions);
       const response = await chai.request(app).get('/missions');
 
       expect(response.status).to.be.equal(200);
       expect(response.body).to.haveOwnProperty('missions');
       expect(response.body.missions).to.be.instanceOf(Array);
+      expect(response.body.missions).to.have.lengthOf(3);
+
+      sinon.restore();
     });
   });
-
+  
   describe('POST /missions', function () {
+    beforeEach(function () {
+      sinon.stub(fs.promises, 'writeFile')
+      .resolves();
+    });
+    afterEach(sinon.restore);
+    
+    const mockMission = {
+      name: 'Trybe',
+      year: '2022',
+      country: 'Brasil',
+      Destination: 'Titã',
+    };
     it('retorna a missão criada com um id', async function () {
-      const mockMission = {
-        name: 'Trybe',
-        year: '2022',
-        country: 'Brasil',
-        Destination: 'Titã',
-      };
-      
       const response = await chai.request(app).post('/missions').send(mockMission);
 
       expect(response.status).to.be.equal(201);
@@ -36,6 +55,11 @@ describe('Rota de Missões', function () {
       expect(response.body.mission.year).to.equal(mockMission.year);
       expect(response.body.mission.country).to.equal(mockMission.country);
       expect(response.body.mission.Destination).to.equal(mockMission.Destination);
+    });
+
+    it('Escreve a nova missão no arquivo de missões', async function () {
+      await chai.request(app).post('/missions').send(mockMission);
+      expect(fs.promises.writeFile.called).to.be.equal(true);
     });
   });
 });
--- tests/unit/fsUtils.test.js
+++ tests/unit/fsUtils.test.js
@@ -1,10 +1,23 @@
 const { expect } = require('chai');
 
+const fs = require('fs');
+const sinon = require('sinon');
 const { readMissionsData } = require('../../src/utils/fsUtils');
 
+const mockMissions = JSON.stringify([
+  { id: 1, name: 'Mariner 2', year: '1962', country: 'USA', destination: 'Vênus' },
+  { id: 2, name: 'Venera 4', year: '1967', country: 'URSS', destination: 'Vênus' },
+  { id: 3, name: 'Mariner 5', year: '1967', country: 'USA', destination: 'Vênus' },
+]);
+
 describe('A função readMissionData', function () {
-  it('retornar um array', async function () {
+  it('retornar um array com todos os elementos do arquivo json', async function () {
+    sinon.stub(fs.promises, 'readFile')
+      .resolves(mockMissions);
     const missions = await readMissionsData();
     expect(missions).to.be.instanceOf(Array);
+    expect(missions).to.have.lengthOf(3);
+
+    sinon.restore();
   });
 });
